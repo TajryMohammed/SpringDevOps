2afd7cd26dc6aa4a4c00438c66d923bf6bc32902
TajryMohammed <tajry.mohammed@gmail.com>
2025-07-26 17:52:32 +0100

    latest update

---
+++ src/main/java/org/tajry/devopstpjenkins/DevOpsTPjenkinsApplication.java
@@ -0,0 +1,16 @@
+package org.tajry.devopstpjenkins;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DevOpsTPjenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOpsTPjenkinsApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/org/tajry/devopstpjenkins/DevOpsTPjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tajry.devopstpjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsTPjenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
